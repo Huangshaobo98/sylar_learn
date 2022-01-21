f917b520445dc9fa9cccdc4ee8fdc5c16bef4b55
Huangshaobo98 <85424396+Huangshaobo98@users.noreply.github.com>
2022-01-21 11:36:46 +0800

    Create endian.h

---
+++ src/endian.h
@@ -0,0 +1,51 @@
+#ifndef __ENDIAN_H__
+#define __ENDIAN_H__
+
+#include <byteswap.h>
+#include <stdint.h>
+
+namespace sylar {
+
+    template<typename T>
+    typename std::enable_if<sizeof(T) == sizeof(uint64_t), T>::type
+    byteswap(T value) {
+        return (T)bswap_64((uint64_t)value);
+    }
+
+    template<typename T>
+    typename std::enable_if<sizeof(T) == sizeof(uint32_t), T>::type
+    byteswap(T value) {
+        return (T)bswap_32((uint32_t)value);
+    }
+
+    template<typename T>
+    typename std::enable_if<sizeof(T) == sizeof(uint16_t), T>::type
+    byteswap(T value) {
+        return (T)bswap_16((uint16_t)value);
+    }
+
+    #if __BYTE_ORDER == __BIG_ENDIAN
+
+    template<typename T>
+    T byteswapOnLittleEndian(T t) {
+        return t;
+    }
+
+    template<typename T>
+    T byteswapOnBigEndian(T t) {
+        return byteswap(t);
+    }
+
+    #else
+    template<typename T>
+    T byteswapOnLittleEndian(T t) {
+        return byteswap(t);
+    }
+
+    template<typename T>
+    T byteswapOnBigEndian(T t) {
+        return t;
+    }
+    #endif
+}
+#endif
